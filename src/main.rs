4a5a9f85e786cf7564b11f2398348059f0714936
alterwain <alterwain@protonmail.com>
2025-03-11 15:59:44 +0300

    modified:   Cargo.lock     modified:   Cargo.toml 	deleted:    public_release 	deleted:    sign_in 	modified:   src/main.rs 	renamed:    html -> src/www/html 	renamed:    loading_screen -> src/www/loading_screen 	renamed:    log_screen -> src/www/log_screen 	renamed:    logged_in -> src/www/logged_in 	renamed:    main_screen -> src/www/main_screen 	renamed:    profile_customization -> src/www/profile_customization 	new file:   src/www/public_release 	renamed:    settings -> src/www/settings 	new file:   src/www/sign_in.html

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,41 @@
-use web_view::Content;
+use winit::application::ApplicationHandler;
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::event::{Event, WindowEvent};
+use winit::platform::run_on_demand::EventLoopExtRunOnDemand;
+use winit::window::{Window, WindowId};
+use winit::event_loop::ActiveEventLoop;
+use wry::WebViewBuilder;
+
+#[derive(Default)]
+struct App {
+  window: Option<Window>,
+  webview: Option<wry::WebView>,
+}
+
+impl ApplicationHandler for App {
+  fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+    let window = event_loop.create_window(Window::default_attributes()).unwrap();
+    let webview = WebViewBuilder::new()
+      .with_html(include_str!("www/sign_in.html"))
+      .build(&window)
+      .unwrap();
+
+    self.window = Some(window);
+    self.webview = Some(webview);
+  }
+
+  fn window_event(&mut self, event_loop: &ActiveEventLoop, _window_id: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
+            }
+            _ => {}
+        }
+  }
+}
 
 fn main() {
-    let html_content = String::from_utf8(std::fs::read("main_screen").unwrap()).unwrap();
-	
-    web_view::builder()
-        .title("My Project")
-        .content(Content::Html(html_content))
-        .size(800, 600)
-        .resizable(false)
-        .debug(true)
-        .user_data(())
-        .invoke_handler(|_webview, _arg| Ok(()))
-        .run()
-        .unwrap();
+    let event_loop = EventLoop::new().unwrap();
+    let mut app = App::default();
+    event_loop.run_app(&mut app).unwrap();
 }
